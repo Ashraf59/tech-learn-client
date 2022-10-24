76c08b61389129476ca738e6e787ca1d978711b6
Ashraf59 <ashrafams59b@gmail.com>
2022-10-24 22:31:39 +0600

    Tech Learn Website

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkANVmK1Gq8JJpv1Vc3T0KhhiNmLJW2BQ",
+  authDomain: "tech-learn-7ef7f.firebaseapp.com",
+  projectId: "tech-learn-7ef7f",
+  storageBucket: "tech-learn-7ef7f.appspot.com",
+  messagingSenderId: "459152339915",
+  appId: "1:459152339915:web:340da43fee69ab47e7bc4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
